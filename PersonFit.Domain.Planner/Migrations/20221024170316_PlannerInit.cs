fd60306f6ecbaf28dac74ee82c284dcf64274d5d
Paweł Haracz <pawel.haracz.93@gmail.com>
2022-10-24 19:06:36 +0200

    Add migration

---
+++ PersonFit.Domain.Planner/Migrations/20221024170316_PlannerInit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PersonFit.Domain.Planner.Infrastructure.Postgres.Documents;
+
+#nullable disable
+
+namespace PersonFit.Domain.Planner.Migrations
+{
+    public partial class PlannerInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exercises",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false),
+                    ExerciseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Repetitions = table.Column<IEnumerable<RepetitionDao>>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercises", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Planners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false),
+                    DailyPlanners = table.Column<IEnumerable<DailyPlannerDao>>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exercises");
+
+            migrationBuilder.DropTable(
+                name: "Planners");
+        }
+    }
+}
---
+++ PersonFit/README.md
@@ -0,0 +1,18 @@
+# PersonFit
+
+Ustrukturyzowany monolit składający się z modułów
+- Exercise 
+- Planner
+
+## Migracja 
+Migracje tworzy się za pomocą komendy:
+
+```bash
+dotnet ef migrations add PlannerInit --context PostgresPlannerDomainContext -p PersonFit.Domain.Planner --startup-project PersonFit
+```
+
+## Lokalny środowisko
+```bash
+cd PersonFit/infra
+docker-compose up 
+```
