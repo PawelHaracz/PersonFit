a771379157847eee47f900a403572051fae1f37c
Paweł Haracz <pawel.haracz.93@gmail.com>
2022-10-26 20:54:30 +0200

    add missing migration

---
+++ PersonFit.Domain.Exercise/Migrations/20221026184352_ExceriseInit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PersonFit.Domain.Exercise.Infrastructure.Postgres.Documents;
+
+#nullable disable
+
+namespace PersonFit.Domain.Exercise.Migrations
+{
+    public partial class ExceriseInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "exercise");
+
+            migrationBuilder.CreateTable(
+                name: "exercises",
+                schema: "exercise",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    tags = table.Column<IEnumerable<string>>(type: "jsonb", nullable: true),
+                    media = table.Column<IEnumerable<Media>>(type: "jsonb", nullable: true),
+                    version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_exercises", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "exercises",
+                schema: "exercise");
+        }
+    }
+}
---
+++ PersonFit.Domain.Planner/Migrations/20221026184254_PlannerInit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PersonFit.Domain.Planner.Infrastructure.Postgres.Documents;
+
+#nullable disable
+
+namespace PersonFit.Domain.Planner.Migrations
+{
+    public partial class PlannerInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "planner");
+
+            migrationBuilder.CreateTable(
+                name: "planner_exercises",
+                schema: "planner",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    version = table.Column<int>(type: "integer", nullable: false),
+                    exercise_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    owner_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    repetitions = table.Column<IEnumerable<RepetitionDao>>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_planner_exercises", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "planners",
+                schema: "planner",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    owner_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    start_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    version = table.Column<int>(type: "integer", nullable: false),
+                    daily_planners = table.Column<IEnumerable<DailyPlannerDao>>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_planners", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "planner_exercises",
+                schema: "planner");
+
+            migrationBuilder.DropTable(
+                name: "planners",
+                schema: "planner");
+        }
+    }
+}
