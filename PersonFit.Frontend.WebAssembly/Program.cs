59fc891432654068908d7943e14f98dfcf098a5d
cdpipeline <cdpipeline@ailleron.com>
2023-02-01 22:20:57 +0100

    Add webassembly blazor

---
+++ PersonFit.Frontend.WebAssembly/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PersonFit.Frontend.WebAssembly;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes
+        .Add("https://personfit.onmicrosoft.com/6d9b6497-15fc-4aa6-8e23-0525b5229548/user_impersonation");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+    options.ProviderOptions.LoginMode = "redirect";
+    options.AuthenticationPaths.LogOutSucceededPath = "";
+});
+
+await builder.Build().RunAsync();
